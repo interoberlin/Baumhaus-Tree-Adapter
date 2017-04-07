e91ae9ed81f5d32c30b9fb5bf6cc608f5238bb49
Matthias <mail@matthiasbock.net>
2017-04-07 21:23:38 +0200

    added pinout header file

---
+++ pinout.h
@@ -0,0 +1,20 @@
+/**
+ * Microcontroller pin functions
+ */
+
+#ifndef PINOUT_H
+#define PINOUT_H
+
+#define PIN_LED_DATA1       21
+#define PIN_LED_DATA2       23
+#define PIN_LED_DATA3       24
+#define PIN_LED_DATA4       22
+
+#define PIN_ATX_ON          6
+#define PIN_ATX_OK          4
+
+#define PIN_FUSE_OK         18
+
+#define PIN_CURRENT_SENSOR  25
+
+#endif // PINOUT_H
